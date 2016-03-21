50df413d10c89e81fe7c747cf6d27c1b82b3d564
ZanW <ZanW@users.noreply.github.com>
2016-03-21 16:14:02 -0400

    chapter3_3-2

---
+++ chapter3_3-2/GradeBook.java
@@ -0,0 +1,12 @@
+package chapter3_3$2;
+
+//create class Gradebook to display a message with medhod displayMEssage
+
+public class GradeBook {
+	
+	public void display() {
+		
+		System.out.println("welcome th the Graade Book!");
+	}
+
+}
---
+++ chapter3_3-2/GradeBookTest.java
@@ -0,0 +1,15 @@
+package chapter3_3$2;
+
+public class GradeBookTest {
+	
+	public static void main(String[] args) {
+		
+//		String string = "I love programming";
+	
+	GradeBook test = new GradeBook();
+	
+	test.display();
+	
+	}
+
+}
