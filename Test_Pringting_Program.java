e9fbe3f9df0352454a3c55e757c7ba7c2230a627
ZanW <ZanW@users.noreply.github.com>
2016-04-25 11:57:11 -0400

    introduction_to_java_application

---
+++ Test_Pringting_Program.java
@@ -0,0 +1,16 @@
+package introduction_to_java_application;
+
+/**
+ *  Welcome to java 
+ *  Text-printing program 
+ */
+
+public class Test_Pringting_Program {
+	
+	// main method begins the application of Java Programming
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Java Programming");
+	}// end method main
+
+}//end class Test_Pringting_Program
